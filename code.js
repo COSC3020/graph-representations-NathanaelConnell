a39ab310fdb53c17c616f6ad0dac0f5a971dbc37
Nathanael Connell <nathanael@Nathanaels-MacBook-Pro.local>
2024-10-30 16:28:23 -0600

    First Commit

--- code.js
+++ code.js
@@ -1,3 +1,20 @@
 function convertToAdjList(adjMatrix) {
-    return adjMatrix;
+
+    if (adjMatrix == []) {
+        return [];
+    }
+
+    var adjList = [];
+    for (var i = 0; i < adjMatrix.length; i++) {
+        
+        adjNodes = [];
+        for (var j = 0; j < adjMatrix[i].length; j++) {
+            if (adjMatrix[i][j] == 1) { //checks if there is an edge between i and j
+                adjNodes.push(j);
+            }
+        }
+        adjList.push(adjNodes);
+    }
+
+    return adjList;
 }
